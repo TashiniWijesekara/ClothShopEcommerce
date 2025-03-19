c71c50cb41e0545f7a82bfe4e3e64d29b67fe2ed
TashiniWijesekara <wijesekaratashini@gmail.com>
2025-03-19 15:28:21 +0530

    update

---
+++ frontend/src/components/Offer.tsx
@@ -0,0 +1,29 @@
+const Offer = () => {
+    return (
+        <section className='bg-banneroffer bg-cover bg-center  px-2 py-2 mt-1'>
+            <div className='max_padd_container flex flex-col items-end text-white'>
+            </div>
+            <section className='bg-banneroffer1 bg-cover bg-center w-full px-2 py-24 '>
+                <div className='max_padd_container flex flex-col items-end text-white'>
+                    <br/>
+                    <br/><br/><br/><br/>
+                    <h3 className='h3'> </h3>
+                    <h3 className='h3 capitalize'> </h3>
+                    <button className='btn_rounded'> </button>
+                </div>
+            </section>
+            <section className='bg-banneroffer2 bg-cover bg-center w-full px-2 py-24 mt-1 '>
+                <div className='max_padd_container flex flex-col items-end text-white'>
+                    <br/>
+                    <br/>
+                    <br/><br/><br/><br/>
+                    <h3 className='h3'> </h3>
+                    <h3 className='h3 capitalize'> </h3>
+                    <button className='btn_rounded'> </button>
+                </div>
+            </section>
+        </section>
+    );
+};
+
+export default Offer;
